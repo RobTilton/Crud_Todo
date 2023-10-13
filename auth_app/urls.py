362371228f2ecf32d00b139a235e30768625876d
Robert T <RobertJTilton@gmail.com>
2023-10-13 10:39:22 -0400

    set up views, templates, urls for auth placeholder

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = 'auth_app'
+
 urlpatterns = [
-    path('', views.Home, name='home'),
-    path('login/', views.Login, name='login' ),
-    
+   path('', views.Home, name='home'),
+   path('home/', views.Home, name='home'),
+   path('login/', views.Login, name='login' ),
+   path('logout/', views.Logout, name='logout'),
+   path('registration/', views.Registration, name='registration'),
+
 ]
--- auth_app/views.py
+++ auth_app/views.py
@@ -1,9 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 from . import models
-# Create your views here.
 
 def Home(request):
     return render(request, 'home.html')
 
 def Login(request):
     return render(request, 'login.html')
+    
+
+def Logout(request):
+    return render(request, 'logout.html')
+
+
+def Registration(request):
+    return render(request , 'registration.html')
