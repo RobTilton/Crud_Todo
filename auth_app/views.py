ebdedf30e73b69c660d52be12f0057b1c8707f4c
Robert T <RobertJTilton@gmail.com>
2023-10-13 13:57:11 -0400

    Login, Logout complete

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,17 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
 from django.shortcuts import render, redirect
 from . import models
 
 def Home(request):
     return render(request, 'home.html')
-
-def Login(request):
-    return render(request, 'login.html')
-    
-
-def Logout(request):
-    return render(request, 'logout.html')
 
 
 def Registration(request):
@@ -21,6 +14,31 @@ def Registration(request):
             user = form.save()
             login(request, user)
             return redirect('home')
+    
     else:
         form = UserCreationForm()
+
     return render(request , 'registration.html', {'form': form})
+
+
+def Login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user != None:
+                login(request, user)
+                return redirect('home')
+    
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'login.html', {'form': form})
+
+    
+
+def Logout(request):
+    logout(request)
+    return redirect('login')
