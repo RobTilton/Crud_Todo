975aee5010364fff054d938ce84f5f563a53da1b
Robert T <RobertJTilton@gmail.com>
2023-10-18 12:54:41 -0400

    new Schema

--- todoList_app/models.py
+++ todoList_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class TodoList(models.Model):
+    name = models.CharField(max_length = 200)
+    user = models.ForeignKey(User, on_delete = models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class TodoItem(models.Model):
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+    todo_list = models.ForeignKey(TodoList, on_delete = models.CASCADE)
+
+    # optional fields for weather api idea.
+    date = models.DateTimeField(null=True, blank=True)
+    address = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.name
