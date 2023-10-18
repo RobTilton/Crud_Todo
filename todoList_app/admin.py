ba3396374ef138ce362887d6c8adc74ebab4196d
Robert T <RobertJTilton@gmail.com>
2023-10-18 12:39:13 -0400

    Removal of current DB Schema

--- todoList_app/admin.py
+++ todoList_app/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-from .models import UserProfile, TodoList, TodoItem
-
-admin.site.register(UserProfile)
-admin.site.register(TodoList)
-admin.site.register(TodoItem)
--- todoList_app/models.py
+++ todoList_app/models.py
@@ -1,29 +1,3 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    owned_tables = models.ManyToManyField('TodoList', related_name='owners')
-
-    def __str__(self):
-        return self.user.username
-
-class TodoList(models.Model):
-    owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    tableNumber = models.AutoField(primary_key=True, unique=True)  # `tableNumber` is PKey.
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.name} (List number {self.tableNumber})"
-
-class TodoItem(models.Model):
-    list = models.ForeignKey(TodoList, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
-    DoByDate = models.DateTimeField(null=True, blank=True)
-    Location = models.CharField(max_length=255, blank=True)
-    Needs = models.TextField(blank=True)
-    Additional = models.TextField(blank=True)
-
-    def __str(self):
-        return self.name
-
